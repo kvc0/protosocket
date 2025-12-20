bb466867c5fcd5b384d7a5f88920028aa7841033
kvc0 <3454741+kvc0@users.noreply.github.com>
2025-12-19 17:02:30 -0800

    spillway docs

--- spillway/src/lib.rs
+++ spillway/src/lib.rs
@@ -1,3 +1,6 @@
+#![deny(missing_docs)]
+#![doc = include_str!("../README.md")]
+
 mod receiver;
 mod sender;
 mod shared;
@@ -7,6 +10,7 @@ use std::sync::Arc;
 pub use receiver::Receiver;
 pub use sender::Sender;
 
+/// Get a new spillway channel with a default concurrency level.
 pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
     // const PARALLELISM: std::sync::LazyLock<usize> = std::sync::LazyLock::new(|| {
     //     std::thread::available_parallelism()
@@ -17,6 +21,12 @@ pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
     channel_with_concurrency(8)
 }
 
+/// Get a new spillway channel with the given concurrency level.
+///
+/// Use this when you need lots of parallelism, or when you know how many Senders
+/// you will have. Higher numbers reduce contention, but increase the cost of
+/// parking the Receiver when idle. Thread count is a good starting point for
+/// concurrency.
 pub fn channel_with_concurrency<T>(concurrency: usize) -> (Sender<T>, Receiver<T>) {
     let shared = Arc::new(shared::Shared::new(concurrency));
     let sender = Sender::new(shared.clone());
